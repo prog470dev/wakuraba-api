45e17736eebe8542a4aa30084cf7b698fd1b8092
maida <brobrokokko@gmail.com>
2018-01-19 17:27:55 +0900

    修正

--- resist_comments.php
+++ resist_comments.php
@@ -3,8 +3,15 @@
 $landmark_id=$_GET['landmark_id'];
 $tweet_id=$_GET['tweet_id'];
 
-$s=mysql_connect("homesec","homesec","password",'');
-mysql_select_db("homesec",$s);
-mysql_query("INSERT INTO tweet_table(landmark_id,name) VALUES($landmark_id,$tweet_id)");
-mysql_close($s);
+$dsn='mysql:dbname=homesec;host=homesec';
+$user='homesec';
+$password='password';
+$dbh=new PDO($dsn,$user,$password);
+$dbh->query('SET NAMES utf8');
+.
+//データベースへ接続
+$sql='INSERT INTO tweet_table(tweet_id,name)VALUES("'.$landmark_id.'","'.$tweet_id.'")';
+$stmt=$dbh->prepare($sql);
+$stmt->execute();
+$dbh=null;
 ?>
