ca30d6dec2f0f63c0c02623a85249224fb8c0b3d
maida <brobrokokko@gmail.com>
2018-01-19 16:58:15 +0900

    修正4

--- get_surround_tag.php
+++ get_surround_tag.php
@@ -14,7 +14,8 @@ $dbh=new PDO($dsn,$user,$password);
 $dbh->query('SET NAMES utf8');
 
 //データベースへ接続
-$sql='SELECT*FROM landmark_table WHERE latitude = $lsti AND longitude = $longi AND tag = $tag';
+$sql='SELECT*FROM landmark_table WHERE ABS(latitude - $lsti) <= $ran AND ABS(longitude - $longi) <= $ran AND tag like "$tag%"
+';
 $stmt=$dbh->prepare($sql);
 $stmt->execute();
 
