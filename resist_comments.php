c811779d987143d3f2e7b86689de81fe483ff312
maida <brobrokokko@gmail.com>
2018-01-19 17:10:22 +0900

    修正

--- resist_comments.php
+++ resist_comments.php
@@ -4,7 +4,8 @@ $landmark_id=$_GET['landmark_id'];
 $tweet_id=$_GET['tweet_id'];
 
 $s=mysql_connect("homesec","homesec","password",'');
-mysql_select_db("tweet_table",$s);
-mysql_query("INSERT INTO regist(landmark_id,name) VALUES($landmark_id,$tweet_id)");
+mysql_select_db("homesec",$s);
+mysql_query("INSERT INTO tweet_table(landmark_id,name) VALUES($landmark_id,$tweet_id)");
+print "成功です"
 mysql_close($s);
 ?>
