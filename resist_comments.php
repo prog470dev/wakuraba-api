0249b1b20b2d1da488f222f18bb1a271f55d2988
maida <brobrokokko@gmail.com>
2018-01-19 17:11:59 +0900

    変更

--- resist_comments.php
+++ resist_comments.php
@@ -6,6 +6,5 @@ $tweet_id=$_GET['tweet_id'];
 $s=mysql_connect("homesec","homesec","password",'');
 mysql_select_db("homesec",$s);
 mysql_query("INSERT INTO tweet_table(landmark_id,name) VALUES($landmark_id,$tweet_id)");
-print "成功です"
 mysql_close($s);
 ?>
