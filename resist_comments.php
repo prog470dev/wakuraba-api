6ee563fa3a49e10328b519fd6826b855bf067155
maida <brobrokokko@gmail.com>
2018-01-19 19:58:21 +0900

    修正

--- resist_comments.php
+++ resist_comments.php
@@ -1,17 +1,19 @@
-<?php
-//ランドマークIDとツイートIDの登録
-$landmark_id=$_GET['landmark_id'];
-$tweet_id=$_GET['tweet_id'];
+<<?php
+	//コメント登録
+	$lan_id=$_GET['landmark_id'];
+	$twe_id=$_GET['tweet_id'];
+	//JSON形式
+	header('Content-type: application/json; charset=utf-8')
+	//接続設定
+	$dsn='mysql:dbname=homesec;host=localhost';
+	$user='homesec';
+	$password='password';
+	$dbh=new PDO($dsn,$user,$password);
+	$dbh->query('SET NAMES utf8');
+	//データベースにコメント登録
+	$sql='INSERT INTO tweet_table(landmark_id,name)VALUES("'.$lan_id.'","'.$twe_id.'")';
+	$stmt=$dbh->prepare($sql);
+	$stmt->execute();
 
-$dsn='mysql:dbname=homesec;host=homesec';
-$user='homesec';
-$password='password';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->query('SET NAMES utf8');
-.
-//データベースへ接続
-$sql='INSERT INTO tweet_table(tweet_id,name)VALUES("'.$landmark_id.'","'.$tweet_id.'")';
-$stmt=$dbh->prepare($sql);
-$stmt->execute();
-$dbh=null;
-?>
+	$dbh=null;
+	?>
