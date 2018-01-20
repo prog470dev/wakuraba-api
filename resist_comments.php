15940d0dfcab0f05ca2cb6e136cedca5ec39e340
prog470dev <shimizutaiki@shimizudaiki-no-MacBook-Pro.local>
2018-01-20 11:15:21 +0900

    もとに戻した。

--- resist_comments.php
+++ resist_comments.php
@@ -1,60 +1,19 @@
 <?php
-//try{
-
-	//JSON形式
-	//header('Content-type: application/json; charset=utf-8')
-
-	//接続設定
-	//$dsn='mysql:dbname=homesec;host=localhost';
-	//$user='homesec';
-	//$password='password';
-	//$dbh=new PDO($dsn,$user,$password);
-	/*
-	$pdh = new PDO(
-		'mysql:dbname=homesec;host=localhost;charset=utf8',
-		'homesec',
-		'password',
-		[
-			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		]
-	);
-	*/
-
 	//コメント登録
-
 	$lan_id=$_GET['landmark_id'];
-	//$lan_id="'".$lan_id."'"
 	$twe_id=$_GET['tweet_id'];
-	$twe_id="'".$twe_id."'";
-
-	echo $twe_id;
-/*
-	echo $lan_id;
-	echo $twe_id;
-*/
-	//$dbh->query('SET NAMES utf8');
-
+	//JSON形式
+	header('Content-type: application/json; charset=utf-8');
+	//接続設定
+	$dsn='mysql:dbname=homesec;host=localhost';
+	$user='homesec';
+	$password='password';
+	$dbh=new PDO($dsn,$user,$password);
+	$dbh->query('SET NAMES utf8');
 	//データベースにコメント登録
-	//$sql="INSERT INTO tweet_table (landmark_id, name) VALUES ($lan_id, $twe_id)";
+	$sql="INSERT INTO tweet_table(landmark_id,name)VALUES("'".$lan_id."'","'".$twe_id."'")";
+	$stmt=$dbh->prepare($sql);
+	$stmt->execute();
 
-	//$stmt=$dbh->prepare($sql);
-	//$stmt->execute();
-
-	//$result_flag = $pdh->query($sql)
-
-	/*
-	$result_flag = $pdh->query("INSERT INTO tweet_table (landmark_id, name) VALUES ($lan_id, $twe_id)")
-	if (!$result_flag) {
-		die('INSERT qury failed.'.mysql_error());
-	}
-	*/
-
-	//$dbh=null;
-
-//} catch (PDOException $e) {
-//	header('Content-Type: text/plain; charset=UTF-8', true, 500);
-//	exit($e->getMessage());
-///}
-
+	$dbh=null;
 ?>
