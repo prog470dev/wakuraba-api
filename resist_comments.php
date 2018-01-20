886049c80a76124d3183a3425f5977eca9f25c6e
prog470dev <shimizutaiki@shimizudaiki-no-MacBook-Pro.local>
2018-01-20 10:21:05 +0900

    modified: resist_comment.php.

--- resist_comments.php
+++ resist_comments.php
@@ -3,12 +3,14 @@
 	$lan_id=$_GET['landmark_id'];
 	$twe_id=$_GET['tweet_id'];
 	//JSON形式
-	header('Content-type: application/json; charset=utf-8')
+	//header('Content-type: application/json; charset=utf-8')
 	//接続設定
+
 	$dsn='mysql:dbname=homesec;host=localhost';
 	$user='homesec';
 	$password='password';
 	$dbh=new PDO($dsn,$user,$password);
+
 	//$dbh->query('SET NAMES utf8');
 	//データベースにコメント登録
 	$sql="INSERT INTO tweet_table (landmark_id, name) VALUES ("'".$lan_id."'","'".$twe_id."'")";
