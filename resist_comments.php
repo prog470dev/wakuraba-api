62131d9eceaea6f302fcca5e43e6a1bae3d4ac0f
prog470dev <shimizutaiki@shimizudaiki-no-MacBook-Pro.local>
2018-01-20 11:20:26 +0900

    commit.

--- resist_comments.php
+++ resist_comments.php
@@ -5,8 +5,10 @@
 	$twe_id="'".$twe_id."'";
 
 	//JSON形式
-	header('Content-type: application/json; charset=utf-8');
+	//header('Content-type: application/json; charset=utf-8');
+
 	//接続設定
+	/*
 	$dsn='mysql:dbname=homesec;host=localhost';
 	$user='homesec';
 	$password='password';
@@ -18,4 +20,26 @@
 	$stmt->execute();
 
 	$dbh=null;
+	*/
+
+	try{
+	  $pdo = new PDO(
+	    'mysql:dbname=homesec;host=localhost;charset=utf8',
+	    'homesec',
+	    'password',
+	    [
+	      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	    ]
+	  );
+
+		$result_flag = $pdo->query("INSERT INTO tweet_table (landmark_id, name) VALUES ($lan_id, $twe_id)");
+			if (!$result_flag) {
+			die('INSERT qury failed.'.mysql_error());
+		}
+
+	} catch (PDOException $e) {
+	  header('Content-Type: text/plain; charset=UTF-8', true, 500);
+	  exit($e->getMessage());
+	}
 ?>
